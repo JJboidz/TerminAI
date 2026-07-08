c7777efdd025b52f19ac35b7dc3968776461b076
2bc705e2 <jnairsoft175@gmail.com>
2026-07-08 13:18:48 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-clear
+# Redirect ALL output from this point onwards to /dev/null
+exec > /dev/null 2>&1
+
 chmod +x terminai.sh
 
-terminai() {
-    ./terminai.sh
-}
+# Add alias silently
+if ! grep -q "alias terminai=" ~/.bashrc; then
+    echo "alias terminai='./terminai.sh'" >> ~/.bashrc
+fi
 
-clear
-echo 'Type "terminai" to start program.'
-echo ''   
+# Reload silently
+source ~/.bashrc   
